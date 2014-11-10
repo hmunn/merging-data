02ddcef8b6bedd585fc1e5516e1eed695e41cde5
hmunn <hmunn@users.noreply.github.com>
2014-11-09 23:19:40 -0800

    First Submission
    Everything should be working/everything works in testing on local
    machine

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+"use strict";
+
+angular.module('AddressBook',[])
+	.controller('AddressController', function($scope) {
+		$scope.employee = pawneeEmployees;
+		$scope.sortCol = 'lastName';
+		
+		$scope.sortBy = function(sortCol){
+			if($scope.sortCol == sortCol){
+				$scope.sortReverse = !$scope.sortReverse;
+			}
+
+			else{
+				$scope.sortCol = sortCol;
+				$scope.sortReverse = false;
+			}
+		};
+		$scope.isSortedBy = function(colName){
+			return colName == $scope.sortCol;
+		};
+	});
